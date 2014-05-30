41e71b5682288067d4156f92e2c1af54c6048d7a
Ray Salemi <ray@bostonlightpress.com>
2014-05-30 09:58:15 -0400

    Fixed 12_UVM_Components `add_test.svh` so that it properly overrides the build_phase() method.

--- 12_UVM_Components/tb_classes/add_test.svh
+++ 12_UVM_Components/tb_classes/add_test.svh
@@ -17,12 +17,17 @@ class add_test extends random_test;
    `uvm_component_utils(add_test);
 
    add_tester tester_h;
-   
+
    function new (string name, uvm_component parent);
       super.new(name,parent);
    endfunction : new
-  
+
+   function void build_phase(uvm_phase phase);
+      tester_h      = new("tester_h", this);
+      coverage_h    = new("coverage_h",    this);
+      scoreboard_h  = new("scoreboard_h",    this);
+   endfunction
+
 endclass
-   
-   
-   
+
+
