7b5a6de55b85a510cdbc361c0e549507753ec2f5
Ray Salemi <ray@raysalemi.com>
2017-06-19 11:41:53 -0400

    Now only use virtual base classes in QUESTA to avoid problems with VCS.

--- 16_Analysis_Ports_In_the_Testbench/tb_classes/base_tester.svh
+++ 16_Analysis_Ports_In_the_Testbench/tb_classes/base_tester.svh
@@ -13,7 +13,12 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
+`ifdef QUESTA
 virtual class base_tester extends uvm_component;
+`else
+class base_tester extends uvm_component;
+`endif
+
 `uvm_component_utils(base_tester)
    virtual tinyalu_bfm bfm;
 
--- 23_UVM_Sequences/tb_classes/tinyalu_base_test.svh
+++ 23_UVM_Sequences/tb_classes/tinyalu_base_test.svh
@@ -13,7 +13,11 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
+`ifdef QUESTA
 virtual class tinyalu_base_test extends uvm_test;
+`else 
+class tinyalu_base_test extends uvm_test;
+`endif
 
    env       env_h;
    sequencer sequencer_h;
