994502a005a17f331c69e45351088b711829861d
Ray Salemi <ray@raysalemi.com>
2021-04-27 07:48:11 -0400

    Add constructor to add_sequence_item.svh
    Apparently UVM 1.2 is serious about requiring a constructor in all classes.

--- 23_UVM_Sequences/tb_classes/add_sequence_item.svh
+++ 23_UVM_Sequences/tb_classes/add_sequence_item.svh
@@ -15,6 +15,10 @@
 */
 class add_sequence_item extends sequence_item;
    `uvm_object_utils(add_sequence_item)
+     
+  function new(input string name = "add_sequence_item");
+    super.new(name);
+  endfunction 
 
    constraint add_only {op == add_op;}
 
